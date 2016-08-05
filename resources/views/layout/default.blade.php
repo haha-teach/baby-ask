26cc5a8ea262831f41ce5dd56b82e15539f170a0
chuan-hao, you <howtomakeaturn@hotmail.com>
2016-08-05 21:29:16 +0800

    og

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,6 +1,11 @@
 <html>
     <head>
         <title>程式嬰兒の發問</title>
+
+        <meta property="og:title" content="程式嬰兒の發問"/>
+        <meta property="og:description" content="給程式新手發問的問答網站！" />
+        <meta property="og:image" content="http://class.turn.tw/wp-content/uploads/2016/07/boy.png" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/css/skeleton-2.0.4/normalize.css">
         <link rel="stylesheet" href="/css/skeleton-2.0.4/skeleton.css">
